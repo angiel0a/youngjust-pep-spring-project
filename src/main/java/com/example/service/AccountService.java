93e1c2a04128b37d785f7a3a0d9f669e4a8c8200
Angie L <youngjustice27811@gmail.com>
2025-04-10 04:06:14 +0000

    User Story: User Registration - Completed

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,25 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+
+@Service
 public class AccountService {
+    private final AccountRepository accountRepository;
+
+    @Autowired
+    public AccountService(AccountRepository accountRepository){
+        this.accountRepository = accountRepository;
+    }
+
+    public Account getAccountByUsername(String username){
+        return accountRepository.findByUsername(username);
+    }
+
+    public void register(Account account){
+        accountRepository.save(account);
+    }
 }
